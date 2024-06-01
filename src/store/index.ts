b8150693d35c22b990681d04055c071b47b86702
segu_g <segusegment@gmail.com>
2024-06-01 23:57:30 +0900

    update index.ts with dot notaiton

--- src/store/index.ts
+++ src/store/index.ts
@@ -30,7 +30,7 @@ import { settingStoreState, settingStore } from "./setting";
 import { presetStoreState, presetStore } from "./preset";
 import { dictionaryStoreState, dictionaryStore } from "./dictionary";
 import { proxyStore, proxyStoreState } from "./proxy";
-import { createPartialStore } from "./vuex";
+import { createDotNotationPartialStore as createPartialStore } from "./vuex";
 import { engineStoreState, engineStore } from "./engine";
 import { filterCharacterInfosByStyleType } from "./utility";
 import {
@@ -196,7 +196,7 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
   },
 
   LOAD_DEFAULT_STYLE_IDS: {
-    async action({ commit, getters }) {
+    async action({ mutations, getters }) {
       let defaultStyleIds = await window.backend.getSetting("defaultStyleIds");
 
       const allCharacterInfos = getters.GET_ALL_CHARACTER_INFOS;
@@ -240,7 +240,7 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
         }),
       ];
 
-      commit("SET_DEFAULT_STYLE_IDS", { defaultStyleIds });
+      mutations.SET_DEFAULT_STYLE_IDS({ defaultStyleIds });
     },
   },
 
@@ -277,17 +277,17 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
         }
       }
     },
-    async action({ commit }, defaultStyleIds) {
-      commit("SET_DEFAULT_STYLE_IDS", { defaultStyleIds });
+    async action({ mutations }, defaultStyleIds) {
+      mutations.SET_DEFAULT_STYLE_IDS({ defaultStyleIds });
       await window.backend.setSetting("defaultStyleIds", defaultStyleIds);
     },
   },
 
   LOAD_USER_CHARACTER_ORDER: {
-    async action({ commit }) {
+    async action({ mutations }) {
       const userCharacterOrder =
         await window.backend.getSetting("userCharacterOrder");
-      commit("SET_USER_CHARACTER_ORDER", { userCharacterOrder });
+      mutations.SET_USER_CHARACTER_ORDER({ userCharacterOrder });
     },
   },
 
@@ -295,8 +295,8 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
     mutation(state, { userCharacterOrder }) {
       state.userCharacterOrder = userCharacterOrder;
     },
-    async action({ commit }, userCharacterOrder) {
-      commit("SET_USER_CHARACTER_ORDER", { userCharacterOrder });
+    async action({ mutations }, userCharacterOrder) {
+      mutations.SET_USER_CHARACTER_ORDER({ userCharacterOrder });
       await window.backend.setSetting("userCharacterOrder", userCharacterOrder);
     },
   },
@@ -315,16 +315,16 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
   },
 
   INIT_VUEX: {
-    async action({ dispatch }) {
+    async action({ actions }) {
       const promises = [];
 
       // 設定ファイルからstoreへ読み込む
-      promises.push(dispatch("HYDRATE_UI_STORE"));
-      promises.push(dispatch("HYDRATE_PRESET_STORE"));
-      promises.push(dispatch("HYDRATE_SETTING_STORE"));
+      promises.push(actions.HYDRATE_UI_STORE());
+      promises.push(actions.HYDRATE_PRESET_STORE());
+      promises.push(actions.HYDRATE_SETTING_STORE());
 
       await Promise.all(promises).then(() => {
-        dispatch("ON_VUEX_READY");
+        actions.ON_VUEX_READY();
       });
     },
   },
@@ -333,8 +333,8 @@ export const indexStore = createPartialStore<IndexStoreTypes>({
     mutation(state, { isMultiEngineOffMode }) {
       state.isMultiEngineOffMode = isMultiEngineOffMode;
     },
-    action({ commit }, isMultiEngineOffMode) {
-      commit("SET_IS_MULTI_ENGINE_OFF_MODE", { isMultiEngineOffMode });
+    action({ mutations }, isMultiEngineOffMode) {
+      mutations.SET_IS_MULTI_ENGINE_OFF_MODE({ isMultiEngineOffMode });
     },
   },
 });
